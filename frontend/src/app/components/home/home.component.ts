9eb27be4b51680a2cd77ce9acee00604dbe39be1
m-gb <maya.goldberg90@gmail.com>
2019-06-08 23:25:28 +0300

    Add home page

---
+++ frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { BookService, IBook } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  books: IBook[];
+
+  constructor(private titleService: Title, private bs: BookService) { }
+
+  ngOnInit() {
+    this.titleService.setTitle('Book Worm');
+    this.bs.getBooks().subscribe((data: IBook[]) => {
+      this.books = data;
+    });
+  }
+
+  updateTitle(title: string): string {
+    return this.bs.editTitle(title);
+  }
+}
