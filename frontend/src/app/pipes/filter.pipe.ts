81efcff8709f7a64954fdf3f48333c1995ae149a
m-gb <maya.goldberg90@gmail.com>
2019-06-10 19:22:04 +0300

    Update filter pipe to filter books without the book service

--- frontend/src/app/pipes/filter.pipe.ts
+++ frontend/src/app/pipes/filter.pipe.ts
@@ -1,13 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { IBook } from 'src/app/services/book.service';
-import { BookService } from 'src/app/services/book.service';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  constructor(private bs: BookService) { }
+  constructor() { }
 
   // filters all books based on search input
   transform(books: IBook[], searchText: string): IBook[] {
@@ -20,8 +19,8 @@ export class FilterPipe implements PipeTransform {
     }
     else {
       return books.filter((b: IBook) => {
-        const input = searchText.toLocaleLowerCase();
-        const book = this.bs.editTitle(b.title).toLocaleLowerCase();
+        const input = searchText.toLowerCase();
+        const book = b.title.toLowerCase();
         return book.includes(input);
       });
     }
